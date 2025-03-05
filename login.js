57851406c7518aa3ef319500225875d87dcda964
WojciechTechstarter <wojciech.olakowski@tn.techstarter.de>
2025-03-05 15:55:35 +0100

    update login

--- login.js
+++ login.js
@@ -5,12 +5,25 @@
 
 
 function onLogin() {
+
     const emailTI = document.getElementById('emailti').value;
     const password = document.getElementById('passwordti').value;
     console.log('My Credentials: email:', emailTI, 'password:', password);
     alert(
         `Der Bneutzer hat sich erflogreich mit ${emailTI} und ${password} angemeldet`
     )
+
+    const box = document.getElementById('box')
+    console.log(box.style.backgroundColor)
+    if (box.style.backgroundColor === 'darkgrey' || box.style.backgroundColor === '') {
+        box.style.backgroundColor = 'blue'
+    } else {
+        box.style.backgroundColor = 'darkgrey'
+    }
+
+
+
+
 }
 
 
